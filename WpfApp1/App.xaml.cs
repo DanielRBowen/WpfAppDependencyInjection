44aabb09587d8c32ec840b3925e12c34660c4b0d
Daniel <daniel@bpapos.com>
2022-03-28 15:02:00 -0600

    Add project files.

---
+++ WpfApp1/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly ServiceProvider _serviceProvider;
+
+        /// <summary>
+        /// https://executecommands.com/dependency-injection-in-wpf-net-core-csharp/ 
+        /// </summary>
+        public App()
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddTransient<DemoService>();
+            services.AddSingleton<MainWindow>();
+        }
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = _serviceProvider.GetService<MainWindow>();
+
+            if (mainWindow != null)
+            {
+                mainWindow.Show();
+            }
+        }
+    }
+}
---
+++ WpfApp1/DemoService.cs
@@ -0,0 +1,10 @@
+﻿namespace WpfApp1
+{
+    public class DemoService
+    {
+        public string GetJunk()
+        {
+            return "Junk";
+        }
+    }
+}
